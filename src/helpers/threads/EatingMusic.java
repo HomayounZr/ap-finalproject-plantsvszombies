00396759e5fbbef2010a5cb7f449fc8c9ff108e4
saeed <saeedshafie@aut.ac.ir>
2021-01-29 17:19:35 +0330

    Update on AudioPlay

---
+++ src/helpers/threads/EatingMusic.java
@@ -0,0 +1,32 @@
+package helpers.threads;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+
+
+import javax.naming.directory.AttributeInUseException;
+
+public class EatingMusic implements Runnable{
+
+    @Override
+    public void run() {
+
+        try {
+            AudioPlayer t1 = new AudioPlayer("Resource/audio/chomp.mp3");
+            Thread t =new Thread(t1);
+            t.start();
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
+
--- src/helpers/threads/ZombieLogicalThread.java
+++ src/helpers/threads/ZombieLogicalThread.java
@@ -41,4 +41,50 @@ public class ZombieLogicalThread implements Runnable {
             ex.printStackTrace();
         }
     }
+    //Some Spark In The Mind , Not Clearly sure How Works
+    class GroanThread implements Runnable{
+        public int label = 0;
+
+        @Override
+        public void run() {
+
+            while(true)
+            {
+                try {
+                    Thread.sleep(12000+(int)(Math.random()*(10000-6000+1)));
+                    label = (int)(Math.random()*6);
+                    switch (label) {
+                        case 0:
+                            AudioPlayer t1 = new AudioPlayer("Resource/audio/groan1.mp3");
+                            Thread T1 =new Thread(t1);
+                            T1.start();break;
+                        case 1:
+                            AudioPlayer t2 = new AudioPlayer("Resource/audio/groan2.mp3");
+                            Thread T2 =new Thread(t2);
+                            T2.start();break;
+                        case 2:
+                            AudioPlayer t3 = new AudioPlayer("Resource/audio/groan3.mp3");
+                            Thread T3 =new Thread(t3);
+                            T3.start();break;
+                        case 3:
+                            AudioPlayer t4 = new AudioPlayer("Resource/audio/groan4.mp3");
+                            Thread T4 =new Thread(t4);
+                            T4.start();break;
+                        case 4:
+                            AudioPlayer t5 = new AudioPlayer("Resource/audio/groan5.mp3");
+                            Thread T5 =new Thread(t5);
+                            T5.start();break;
+                        case 5:
+                            AudioPlayer t6 = new AudioPlayer("Resource/audio/groan6.mp3");
+                            Thread T6 =new Thread(t6);
+                            T6.start();break;
+                    }
+                }catch(InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+
+        }
+
+    }
 }
